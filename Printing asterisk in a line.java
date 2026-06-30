13cd9bab8e7a7c652ae61df8a9a7f8983a18c6d8
Shambhavicod <reach.shambhavipandey@gmail.com>
2026-06-30 21:03:58 +0530

    Create Printing asterisk in a line

---
+++ Printing asterisk in a line.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    String str = "";
+	    for(int i=1; i<=a; i++){
+	        str = str + "*"; 
+	        }
+	        System.out.print(str);
+	    }
+	}
