285ce4c381a3b3dfc3763e52f2a2297a1ad95c29
Shambhavicod <reach.shambhavipandey@gmail.com>
2026-06-30 20:43:42 +0530

    Printing Triangle according to the user input number.

---
+++ Printing Triangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    String str = "";
+	    for(int i=1; i<=n; i++){
+	        str = str + "*"; 
+	        System.out.println(str);
+	        
+	    }
+	}
+}
